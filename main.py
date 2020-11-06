de65a5235c53551eeba5dff48314484ddc76175a
vamsee <4n0n5ur7@protonmail.com>
2020-11-06 10:22:49 +0530

    added global variables
    Signed-off-by: vamsee <4n0n5ur7@protonmail.com>

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygames
+
+pygames.init()
+
+def main():
+
+    global playerImg
+    global playerX
+    global playerY
+    global playerX_change
+    global bulletY
+    global bulletX
+    global bullet_state
+    global score_value
+    global running
+    global win
